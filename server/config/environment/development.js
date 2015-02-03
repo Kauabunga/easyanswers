a3ce22049a36a1e9740cefd04d1c12701e1c22ca
Carson Bruce <carson.bruce@solnet.co.nz>
2015-02-04 10:25:23 +1300

    Initial commit woowowowowo

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/easyanswers-dev'
+  },
+
+  seedDB: true
+};
